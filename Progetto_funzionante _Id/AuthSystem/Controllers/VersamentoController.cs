5d1e2142dec182ac0d8936af8865a7e9a2db833a
Camilla Franceschini <camilla.franceschini@horsa.it>
2020-09-08 16:37:40 +0200

    Versione 1.3: chiave esterna email

---
+++ Progetto_funzionante _Id/AuthSystem/Controllers/VersamentoController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AuthSystem.Models;
+
+namespace AuthSystem.Controllers
+{
+    public class VersamentoController : Controller
+    {
+        private readonly NContext _context;
+
+        public VersamentoController(NContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Versamento
+        public async Task<IActionResult> Index()
+        {
+            var nContext = _context.Versamenti.Include(v => v.Articoli).Include(v => v.AspNetUsers);
+            return View(await nContext.ToListAsync());
+        }
+
+        // GET: Versamento/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var versamento = await _context.Versamenti
+                .Include(v => v.Articoli)
+                .Include(v => v.AspNetUsers)
+                .FirstOrDefaultAsync(m => m.IdVersamento == id);
+            if (versamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(versamento);
+        }
+
+        // GET: Versamento/Create
+        public IActionResult Create()
+        {
+            ViewData["IdArticolo"] = new SelectList(_context.Articoli, "IdArticolo", "Descrizione");
+            ViewData["IdAspNetUsers"] = new SelectList(_context.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Versamento/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdVersamento,NumeroPezzi,Data,TempoProd,IdArticolo,IdAspNetUsers")] Versamento versamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(versamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdArticolo"] = new SelectList(_context.Articoli, "IdArticolo", "Descrizione", versamento.IdArticolo);
+            ViewData["IdAspNetUsers"] = new SelectList(_context.AspNetUsers, "Id", "Email", versamento.IdAspNetUsers);
+            return View(versamento);
+        }
+
+        // GET: Versamento/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var versamento = await _context.Versamenti.FindAsync(id);
+            if (versamento == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdArticolo"] = new SelectList(_context.Articoli, "IdArticolo", "Descrizione", versamento.IdArticolo);
+            ViewData["IdAspNetUsers"] = new SelectList(_context.AspNetUsers, "Id", "Email", versamento.IdAspNetUsers);
+            return View(versamento);
+        }
+
+        // POST: Versamento/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdVersamento,NumeroPezzi,Data,TempoProd,IdArticolo,IdAspNetUsers")] Versamento versamento)
+        {
+            if (id != versamento.IdVersamento)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(versamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VersamentoExists(versamento.IdVersamento))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdArticolo"] = new SelectList(_context.Articoli, "IdArticolo", "Descrizione", versamento.IdArticolo);
+            ViewData["IdAspNetUsers"] = new SelectList(_context.AspNetUsers, "Id", "Email", versamento.IdAspNetUsers);
+            return View(versamento);
+        }
+
+        // GET: Versamento/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var versamento = await _context.Versamenti
+                .Include(v => v.Articoli)
+                .Include(v => v.AspNetUsers)
+                .FirstOrDefaultAsync(m => m.IdVersamento == id);
+            if (versamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(versamento);
+        }
+
+        // POST: Versamento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var versamento = await _context.Versamenti.FindAsync(id);
+            _context.Versamenti.Remove(versamento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VersamentoExists(int id)
+        {
+            return _context.Versamenti.Any(e => e.IdVersamento == id);
+        }
+    }
+}
---
+++ Progetto_funzionante _Id/AuthSystem/Models/Versamento.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthSystem.Models
+{
+    public class Versamento
+    {
+        [Key]
+        public int IdVersamento { get; set; }
+        [Column(TypeName = "nchar(250)")]
+        [Required(ErrorMessage = "Immettere il numero dei pezzi fabbricati")]
+        [DisplayName("Numero pezzi fabbricati")]
+        public float NumeroPezzi { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd\\-MM\\-yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Immettere la data aggiornata")]
+        [DisplayName("Data versamento")]
+        public DateTime Data { get; set; }
+
+        [Column(TypeName = "numeric")]
+        [DisplayName("Tempo di produzione (Minuti)")]
+        [Required(ErrorMessage = "Immettere il tempo di produzione di tutti gli articoli")]
+        public float TempoProd { get; set; }
+
+        [DisplayName("Nome articolo")]
+        public int IdArticolo { get; set; }
+        [ForeignKey("IdArticolo")]
+        [DisplayName("Nome articolo")]
+        public Articolo Articoli { get; set; }
+
+        [MaxLength(450), ForeignKey("AspNetUsers"),DisplayName("Email") ]
+        public string IdAspNetUsers { get; set; }
+       
+        [DisplayName("Email")]
+        public ApplicationUser AspNetUsers { get; set; }
+
+
+    }
+}
