586ca6ce2fe640c8a8d7bf0209286237fc93dd79
Camilla Franceschini <camilla.franceschini@horsa.it>
2020-09-18 18:17:17 +0200

    Versione 1.9: aggiunta layout grafici

---
+++ AuthSystem/Controllers/GraficiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthSystem.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSystem.Controllers
+{
+    public class GraficiController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new GraficoViewModel();
+
+            viewModel.Labels = new string[] { "Articolo1 ok", "Articolo2 ko" };
+
+            viewModel.DataSet = new int[] { 10, 20 };
+
+            return View(viewModel);
+        }
+    }
+}
