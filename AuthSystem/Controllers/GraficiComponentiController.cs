3ed1dab19d291a854cf153373f96df030b4b1607
Camilla Franceschini <camilla.franceschini@horsa.it>
2020-10-06 17:19:14 +0200

    view Grafici componenti

--- AuthSystem/Controllers/GraficiComponentiController.cs
+++ AuthSystem/Controllers/GraficiComponentiController.cs
@@ -2,18 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AuthSystem.Models;
+using AuthSystem.ViewModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AuthSystem.Controllers
 {
     [Authorize]
     public class GraficiComponentiController : Controller
     {
+        private readonly NContext _context;
+
+        public GraficiComponentiController(NContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var articoli = _context.Articoli.ToList();
 
-            return View();
+            var viewModel = new GraficoComponentiViewModel();
+
+            viewModel.Articoli = new SelectList(articoli, "IdArticolo", "NomeArticolo");
+
+            return View(viewModel);
         }
     }
 }
---
+++ AuthSystem/ViewModel/GraficoComponentiViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AuthSystem.ViewModel
+{
+    public class GraficoComponentiViewModel
+    {
+        public SelectList Articoli { get; set; }
+    }
+}
