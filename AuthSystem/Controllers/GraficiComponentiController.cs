58016d3c9de4f8afc623d17b2de29c52a801d89e
Camilla Franceschini <camilla.franceschini@horsa.it>
2020-10-06 16:48:24 +0200

    Creato controller grafici componenti

---
+++ AuthSystem/Controllers/GraficiComponentiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSystem.Controllers
+{
+    [Authorize]
+    public class GraficiComponentiController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
