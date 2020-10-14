8a1a836259df1b0b42d8dcbd772dbedf624c5f86
Camilla Franceschini <camilla.franceschini@horsa.it>
2020-10-14 09:34:09 +0200

    Unione di GraficoComponentiViewModel e ChartComponentiModel

--- AuthSystem/ViewModel/GraficoComponentiViewModel.cs
+++ AuthSystem/ViewModel/GraficoComponentiViewModel.cs
@@ -5,8 +5,15 @@ namespace AuthSystem.ViewModel
     public class GraficoComponentiViewModel
     {
         public SelectList Articoli { get; set; }
-        
 
-       
+        public string[] Labels { get; set; }
+
+        public int[] Dataset { get; set; }
+
+        public int TempoIdeale { get; set; }
+
+
+
+
     }
 }
