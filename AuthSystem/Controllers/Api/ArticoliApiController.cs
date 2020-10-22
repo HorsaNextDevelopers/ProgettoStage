1c94c4e8d60da61e00f907e607c0030246df7358
Filippo Fiazza <filippo.fiazza@horsa.it>
2020-10-22 16:04:23 +0200

    ArticoliApiController

---
+++ AuthSystem/Controllers/Api/ArticoliApiController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AuthSystem.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticoliApiController : ControllerBase
+    {
+        // GET: api/<ArticoliApiController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ArticoliApiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ArticoliApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ArticoliApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ArticoliApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
