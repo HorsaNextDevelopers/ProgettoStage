5558e5b18a9e06100993dc827f053318fb3e0963
Filippo Fiazza <filippo.fiazza@horsa.it>
2020-10-23 09:44:12 +0200

    prova metodo PUT

--- AuthSystem/Controllers/Api/ArticoliApiController.cs
+++ AuthSystem/Controllers/Api/ArticoliApiController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AuthSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +14,13 @@ namespace AuthSystem.Controllers.Api
     [ApiController]
     public class ArticoliApiController : ControllerBase
     {
+        private readonly NContext _context;
+
+        public ArticoliApiController(NContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/<ArticoliApiController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -34,14 +43,46 @@ namespace AuthSystem.Controllers.Api
 
         // PUT api/<ArticoliApiController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Articolo articolo)
         {
+
+            if (id != articolo.IdArticolo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articolo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticoloExists(articolo.IdArticolo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return NoContent();
         }
 
         // DELETE api/<ArticoliApiController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        private bool ArticoloExists(int id)
+        {
+            return _context.Articoli.Any(e => e.IdArticolo == id);
         }
     }
 }
