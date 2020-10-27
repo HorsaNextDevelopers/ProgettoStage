d1087f781317c3042c631657b9b09d7e917ca4cd
Tudor Busuioc <tudor.busuioc@horsa.it>
2020-10-27 11:10:39 +0100

    VersamentiApi

--- AuthSystem/Controllers/Api/VersamentiApiController.cs
+++ AuthSystem/Controllers/Api/VersamentiApiController.cs
@@ -77,6 +77,7 @@ namespace AuthSystem.Controllers.Api
             _context.Entry(versamento).State = EntityState.Modified;
 
             try
+
             {
                 await _context.SaveChangesAsync();
             }
